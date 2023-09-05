e8a2b9f0c46ac55d59cb01900b6b1d0b8cefd47a
John Carlo A. Ylanan <138417283+FDC-Johncarlo@users.noreply.github.com>
2023-09-05 22:34:50 +0800

    Update theme responsive

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import { AUTHOR_NAME } from "@/const/author";
 
@@ -12,15 +13,17 @@ const Sidebar: React.FC<sideBarProps> = ({ styles }) => {
     return (
         <aside className={styles.sidebar}>
             <div className={styles.author}>
-                <Image
-                    height={50}
-                    width={50}
-                    alt="Author Profile"
-                    src="/images/author.jpg" />
-                <div className={styles.details}>
-                    <h3>{AUTHOR_NAME}</h3>
-                    <span>The Author</span>
-                </div>
+                <Link href="/">
+                    <Image
+                        height={50}
+                        width={50}
+                        alt="Author Profile"
+                        src="/images/author.jpg" />
+                    <div className={styles.details}>
+                        <h3>{AUTHOR_NAME}</h3>
+                        <span>The Author</span>
+                    </div>               
+                </Link>
             </div>
             <div className="label">
                 <hr className="hr-horizontal" />
